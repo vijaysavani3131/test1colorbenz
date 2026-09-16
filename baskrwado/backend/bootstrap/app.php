d5c348b78adc743a7c232bcd6e21076705ce08bb
vijaysavani3131 <131681253+vijaysavani3131@users.noreply.github.com>
2026-09-17 01:30:04 +0530

    security: remove legacy admin key middleware alias

--- baskrwado/backend/bootstrap/app.php
+++ baskrwado/backend/bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\EnsureAdminAuth;
-use App\Http\Middleware\EnsureAdminKey;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,7 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin.auth' => EnsureAdminAuth::class,
-            'admin.key' => EnsureAdminKey::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
